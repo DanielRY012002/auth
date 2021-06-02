12ccf0140b285c9d759d6e5b30a99d49025e5eb6
DanielRY <danielrojasy@upeu.edu.pe>
2021-06-01 22:36:48 -0500

    Auth

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import morgan from 'morgan';
+import userRoutes from './routers/user.routes';
+import productsRoutes from './routers/producto.routes';
+const app=express();
+var cors=require('cors');
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.get('/',function(req,res,next){
+    res.send('Bienvenido a Node JS ....');
+});
+app.use('/api/auth/users',userRoutes);
+app.use('/api/auth/products',productsRoutes);
+export default app;
